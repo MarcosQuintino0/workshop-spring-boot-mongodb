e9135a9c271767c76b5d6785119f3a26c4a46081
Marcos Andre <Marcosquintinodev@gmail.com>
2023-01-10 17:17:58 -0300

    Consulta com varios criterios

--- src/main/java/com/marcosquintino/workshopmongo/services/PostService.java
+++ src/main/java/com/marcosquintino/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.marcosquintino.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,8 +22,13 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 
-	public List<Post> findByTitle(String text){
+	public List<Post> findByTitle(String text) {
 		return repo.searchTitle(text);
 	}
-	
+
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 100);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
+
 }
