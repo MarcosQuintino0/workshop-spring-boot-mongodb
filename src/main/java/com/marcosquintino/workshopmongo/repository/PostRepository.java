7659b625829180c06a9ba71aa220f0d60b8166bd
Marcos Andre <Marcosquintinodev@gmail.com>
2023-01-10 13:56:39 -0300

    Consulta simples com Query methods

--- src/main/java/com/marcosquintino/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/marcosquintino/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.marcosquintino.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.marcosquintino.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
